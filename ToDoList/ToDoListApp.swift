422dd7966629d76b0a170d48882a854be0565b1e
Maya Ghamloush <maya.ghamloushe@gmail.com>
2024-03-19 22:04:01 +0100

    Initial Commit

---
+++ ToDoList/ToDoListApp.swift
@@ -0,0 +1,20 @@
+//
+//  ToDoListApp.swift
+//  ToDoList
+//
+//  Created by Maya Ghamloush on 19/03/2024.
+//
+
+import SwiftUI
+
+@main
+struct ToDoListApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
